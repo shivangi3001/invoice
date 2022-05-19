9638bfee4337b00bde8278f5c6de18c67747d6de
Shivangi Chaurasia <56307595+shivangi3001@users.noreply.github.com>
2022-05-19 14:11:03 +0530

    setting rough framework

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
+  const handlePrint = () => {
+    window.print()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <main>
+       <header>
+         <div>
+           <h2>Invoice</h2>
+         </div>
+
+        <div>
+          <ul>
+            <li><button onClick={handlePrint}>Print</button></li>
+            <li>Download</li>
+            <li>Send</li>
+          </ul>
+        </div>
+
+       </header>
+
+       <section>
+        {/* <input type="text" name="text" id="text" placeholder="your name"
+        required /> */}
+        <h2> Shivangi Chaurasia </h2>
+        <p>your address</p>
+       </section>
+
+       {/* client section */}
+       <section>
+         <h2>name of client</h2>
+         <p>client's address</p>
+       </section>
+
+{/* dates */}
+       <articles>
+         <ul>
+           <li>Invoice number: </li>
+           <li>Invoice date: </li>
+           <li>Due date: </li>
+         </ul>
+       </articles>
+
+{/* table */}
+
+
+
+{/* Notes */}
+        <section>
+          <p> Notes to the client</p>
+        </section>
+
+
+
+        {/* Footer */}
+        <footer>
+          <ul>
+            <li>Your name</li>
+            <li>Your email</li>
+            <li>Phone number</li>
+            <li>Bank</li>
+            <li>Account Number</li>
+          </ul>
+        </footer>
+
+     </main>
+
+    </>
   );
 }
 
